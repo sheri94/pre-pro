05d7244c5a04d6c8e8cc4d89809ad25c6c280e32
sheri94 <eksinon@mail.ru>
2022-05-09 01:54:30 +0500

    kata

---
+++ src/main/java/App.java
@@ -0,0 +1,18 @@
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class App {
+    public static void main(String[] args) {
+        ApplicationContext applicationContext =
+                new AnnotationConfigApplicationContext(AppConfig.class);
+        HelloWorld bean = (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(bean.getMessage());
+
+        HelloWorld bean2 = (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println("Сравенние кошка " + bean.equals(bean2));
+
+        Cat cat1 = (Cat) applicationContext.getBean("cat");
+        Cat cat2 = (Cat) applicationContext.getBean("cat");
+        System.out.println("Сравенние кошка " + cat1.equals(cat2));
+    }
+}
